beb4843eed96739b16af7138825cf7bef1e2a202
Alper Berk Kömpe <kompealper63@gmail.com>
2025-07-18 16:51:54 +0300

    settings is repared

--- src/app/api/settings/route.js
+++ src/app/api/settings/route.js
@@ -1,14 +1,18 @@
 import { getAllNews } from "@/app/services/newsService";
-import { getAllSettings } from "@/app/services/settingsService";
+import { getAllSettings, getAllSettingsPagenation } from "@/app/services/settingsService";
 import { handleApiError } from "@/app/utils/errorHandler";
 
 export async function GET(req) {
   try{
   const  {searchParams} = new URL(req.url);
-  const page = parseInt(searchParams.get("page"))  ||1;
-  const limit = parseInt(searchParams.get("limit")) || 5;
+  const page = parseInt(searchParams.get("page"));
+  const limit = parseInt(searchParams.get("limit"));
 
-  const searchContent = await getAllSettings(page,limit);
+  if(isNaN(page) || isNaN(limit)){
+    const allSettings = await getAllSettings();
+    return Response.json({succes:true,settings:allSettings});
+  }
+  const searchContent = await getAllSettingsPagenation(page,limit);
 
   return Response.json({succes:true,settings : searchContent})
   }catch(error){
--- src/app/services/settingsService.js
+++ src/app/services/settingsService.js
@@ -1,8 +1,13 @@
 import { getDb } from "@/lib/db";
 
-export async function  getAllSettings(page,limit) {
+export async function  getAllSettingsPagenation(page,limit) {
     const db = await getDb();
     const skip = (page - 1) * limit
-    const allSettings = await db.collection("Settings").find({}).skip(skip).limit(limit).toArray();
+    const settings = await db.collection("Settings").find({}).skip(skip).limit(limit).toArray();
+    return settings;
+}
+export async function getAllSettings () {
+    const db = await getDb();
+    const allSettings = await db.collection("Settings").find({}).toArray();
     return allSettings;
 }
