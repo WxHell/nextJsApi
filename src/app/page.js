ee290cd66cb6e19bde6fd02fa5cd993ff6e7fc3f
Alper Berk Kömpe <kompealper63@gmail.com>
2025-07-18 13:53:30 +0300

    homepage is added

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,127 @@
-import Image from "next/image";
+"use client"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+export default  function NewsList() {
+     const [news, setNews] = useState([]);
 
-export default function Home() {
+  useEffect(() => {
+    axios.get("/api/news")
+      .then(res => {
+        setNews(res.data.news || []);
+      })
+      .catch(err => console.error("Haber listeleri alınamadı", err));
+  }, []);
+
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+   
+    <main className="flex min-h-screen flex-wrap ">
+      <div className="flex lg:w-[calc(100%-1140px)] items-center bg-[#FFFFFF] p-8 border-r-4">
+       
+        <div className="text-[#000000]">
+          <div className="fixed top-0 left-0 h-40  w-9 bg-red-600 flex flex-col items-center pt-4">
+            <img src="/image/anaSayfaLogo.png" alt="logo" className=" fixed top-1 left-0 w-9 h-5 z-4 px-1" />
+          </div>
+          
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <div className="text-2xl font [Poppins] opacity-50">26.07.2025</div>
+          <div className="mb-4 text-4xl font-bold font-[AbrilFatface]">
+            Bugün ne oldu?
+          </div>
+          <div className="mb-4 text-[9px] opacity-50">
+            {news.length > 0 ? news[0].summary : "Haber yok"}
+          </div>
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+      </div>
+      <div>           
+        <footer className=" text-xs text-gray-500 absolute bottom-0 left-4 ">
+          <p>
+            Copyright ©️ 2017 – Tüm hakları saklıdır. <br />
+            Habertürk Gazetecilik A.Ş.
+          </p>
+          </footer>
+      </div>
+
+
+      <div className="lg:w-[1140px] bg-white overflow-x-auto  h-screen">
+        <ul className="text-black  text-sm flex flex-row items-stretch justify-center  h-full w-400 overflow-x-auto">
+            
+          <li className="relative border-r border-gray-300 transition duration-300 hover:bg-orange-50 pr-4 flex-1 flex flex-col justify-center items-center items-start ">
+            <div className="px-4 ">
+            <div className="absolute top-4 left-0 font-bold text-4xl pl-4 opacity-25">01</div>
+            <p className="self-start text-xs text-black opacity-50  "> 3 saat önce </p>
+            <p className="mt-2 mb-2 font-bold ">
+              Lorem Ipsum.dkjfşlzkşl şlkdfşlkşlkdf dhshdk sjdkjashdk sjdhakjsdh jsadksj  jsdkhajs jsdkjads jsdkjsdh jadshkjs jhdkasjhk şladkfşlkdf şladkfşkdasş işafsifi lşakdlkdşf askşlksdşl şlaskşlkds alkfsşlksadş laskdjfşlj..
+            </p>
+            <img className="pb-4 " src="/image/bg-image.png" alt="Görsel" width={200} height={120}/>
+            <p className="text-sm mb-2 opacity-50">
+              Lorem Ipsum sjhkjhsjkd sjdhjksdhd jsdhkjdsh sjkdhkjsdhk jsdhksjdh dkhkadhkd jshdkjshk shdkhs sdhdhkhd kjdkdhj jhkjk...
+            </p>
+            <button className="self-start bg-white hover:bg-orange-50  px-4 py-2 font-bold text-black border border-gray-300">DETAY</button>
+           </div>
+          </li>
+
+          <li className="relative border-r border-gray-300 transition duration-300 hover:bg-orange-50 pr-4 flex-1 flex flex-col justify-center items-center items-start">
+            <div className="px-4">
+            <div className="absolute top-4 left-0 font-bold text-4xl opacity-25">02</div>
+            <p className="self-start text-xs text-black opacity-50  "> 3 saat önce </p>
+            <p className="mt-2 mb-2 font-bold ">
+              Lorem Ipsum.dkjfşlzkşl şlkdfşlkşlkdf dhshdk sjdkjashdk sjdhakjsdh jsadksj  jsdkhajs jsdkjads jsdkjsdh jadshkjs jhdkasjhk şladkfşlkdf şladkfşkdasş işafsifi lşakdlkdşf askşlksdşl şlaskşlkds alkfsşlksadş laskdjfşlj..
+            </p>
+            <img className="pb-4" src="/image/17.png" alt="Görsel" width={200} height={120}/>
+            <p className="text-sm mb-2 opacity-50">
+              Lorem Ipsum sjhkjhsjkd sjdhjksdhd jsdhkjdsh sjkdhkjsdhk jsdhksjdh dkhkadhkd jshdkjshk shdkhs sdhdhkhd kjdkdhj jhkjk...
+            </p>
+            <button className="self-start bg-white px-4 py-2 font-bold text-black border border-gray-300">DETAY</button>
+          </div>
+          </li>
+
+          <li className="relative border-r border-gray-300 transition duration-300 hover:bg-orange-50 pr-4 flex-1 flex flex-col justify-center items-center items-start ">
+            <div className="px-4 ">
+            <div className="absolute top-4 left-0 font-bold text-4xl opacity-25">03</div>
+            <p className="self-start text-xs text-black opacity-50  "> 3 saat önce </p>
+            <p className="mt-2 mb-2 font-bold ">
+              Lorem Ipsum.dkjfşlzkşl şlkdfşlkşlkdf dhshdk sjdkjashdk sjdhakjsdh jsadksj  jsdkhajs jsdkjads jsdkjsdh jadshkjs jhdkasjhk şladkfşlkdf şladkfşkdasş işafsifi lşakdlkdşf askşlksdşl şlaskşlkds alkfsşlksadş laskdjfşlj..
+            </p>
+            <img className="pb-4" src="/image/18.png" alt="Görsel" width={200} height={120}/>
+            <p className="text-sm mb-2 opacity-50">
+              Lorem Ipsum sjhkjhsjkd sjdhjksdhd jsdhkjdsh sjkdhkjsdhk jsdhksjdh dkhkadhkd jshdkjshk shdkhs sdhdhkhd kjdkdhj jhkjk...
+            </p>
+            <button className="self-start bg-white px-4 py-2 font-bold text-black border border-gray-300  ">DETAY</button>
+            </div>
+          </li>
+
+          <li className="relative border-r border-gray-300 transition duration-300 hover:bg-orange-50 pr-4 flex-1 flex flex-col justify-center items-center items-start">
+             <div className="px-4 ">
+
+            <div className="absolute top-4 left-0 font-bold text-4xl opacity-25">REKLAM</div>
+            <p className="mt-16 mb-4 ">
+             <img className="pb-4" src="/image/reklam.png" alt="Görsel" width={200} height={120}/>
+            </p>
+            </div>
+          </li>
+
+          <li className="relative border-r border-gray-300 transition duration-300 hover:bg-orange-50 pr-4 flex-1 flex flex-col justify-center items-center items-start ">
+            <div className="absolute top-4 left-0 font-bold text-4xl opacity-25">04</div>
+            <div className="px-4 ">
+            <p className="self-start text-xs text-black opacity-50  "> 3 saat önce </p>
+            <p className="mt-2 mb-2 font-bold ">
+              Lorem Ipsum.dkjfşlzkşl şlkdfşlkşlkdf dhshdk sjdkjashdk sjdhakjsdh jsadksj  jsdkhajs jsdkjads jsdkjsdh jadshkjs jhdkasjhk şladkfşlkdf şladkfşkdasş işafsifi lşakdlkdşf askşlksdşl şlaskşlkds alkfsşlksadş laskdjfşlj..
+            </p>
+            <img className="pb-4" src="/image/19.png" alt="Görsel" width={200} height={120}/>
+            <p className="text-sm mb-2 opacity-50">
+              Lorem Ipsum sjhkjhsjkd sjdhjksdhd jsdhkjdsh sjkdhkjsdhk jsdhksjdh dkhkadhkd jshdkjshk shdkhs sdhdhkhd kjdkdhj jhkjk...
+            </p>
+            <button className="self-start bg-white px-4 py-2 font-bold text-black border border-gray-300">DETAY</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+
+    </main>
+
   );
-}
+    }
