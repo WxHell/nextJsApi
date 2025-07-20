5cc65c11c5c662e04e193a22d617c59bfef9631d
Alper Berk Kömpe <kompealper63@gmail.com>
2025-07-20 13:59:22 +0300

    component is added

--- src/app/pages/categories/[slug]/news/page.js
+++ src/app/pages/categories/[slug]/news/page.js
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import CategoriesMenu from '../../../../components/';
+import CategoriesMenu from '../../../../components/page';
 import Link from 'next/link';
 
 export default function Page() {
