ac3ef1fd626b91a84c5f2ee56eb563ab56c32801
Alper Berk Kömpe <kompealper63@gmail.com>
2025-07-24 14:32:18 +0300

    add is repared

--- src/app/pages/category/[slug]/news/page.js
+++ src/app/pages/category/[slug]/news/page.js
@@ -6,18 +6,16 @@ import { useFetchSlugNews } from '@/app/hooks/useFetch';
 import { fetchCategoryByNews } from '@/app/axioService/allFetchNews';
 
 export default function Page() {
-  const news = [];
-  const {slug} = useParams();
+  const { slug } = useParams();
   const {
-    data:slugNews,
+    data: slugNews,
     loading,
     error,
-  } = useFetchSlugNews(() => slug ? fetchCategoryByNews(slug):Promise.resolve(null),[slug] );
-  if( loading || !news) return null
-  if(error) return <p>Haber alınırken hata oluştu</p>
- 
- 
- 
+  } = useFetchSlugNews(() => slug ? fetchCategoryByNews(slug) : Promise.resolve(null), [slug]);
+
+  if (loading || !slugNews) return null;
+  if (error) return <p>Haber alınırken hata oluştu</p>;
+
   return (
     <main className="flex min-h-screen flex flex row relative">
       <div className="flex w-[400px] items-center bg-[#FFF] p-8 border-r">
@@ -27,7 +25,7 @@ export default function Page() {
             Bugün ne oldu?
           </div>
           <div className="text-[15px] text-[#95989A] font-[Poppins]">
-         {slugNews?.[0]?.summary ?? "Haber Yok"}
+            {slugNews?.[0]?.summary ?? "Haber Yok"}
           </div>
         </div>
         <footer className="text-xs text-[#95989A] absolute bottom-4 font-[Poppins] ">
@@ -40,13 +38,17 @@ export default function Page() {
 
       <div className="w-[calc(100%-400px)] bg-white overflow-x-auto h-screen">
         <ul className="text-black text-sm flex flex-row items-stretch h-full">
-          {slugNews.map((item,index) => (
-            index % 4 === 3 ?
-              <li key={item._id || index} className="relative border-r border-gray-300 px-4 min-w-[280px] max-w-[280px] flex flex-col justify-center px-4">
+          {slugNews.map((item, index) => (
+            index % 4 === 3 ? (
+              <li
+                key={`reklam-${index}`}
+                className="relative border-r border-gray-300 px-4 min-w-[280px] max-w-[280px] flex flex-col justify-center px-4"
+              >
                 <div className="absolute top-4 left-4 font-bold text-4xl opacity-25">REKLAM</div>
-                <img src="/images/reklam.png" alt="Görsel" width={260} height={150}/>
-              </li> : 
-              <li key={item._id || index} className='h-full'>
+                <img src="/images/reklam.png" alt="Görsel" width={260} height={150} />
+              </li>
+            ) : (
+              <li key={`news-${item._id ?? index}`} className="h-full">
                 <NewsCard
                   id={item._id}
                   title={item.title}
@@ -60,14 +62,13 @@ export default function Page() {
                   index={index}
                 />
               </li>
+            )
           ))}
         </ul>
       </div>
     </main>
   );
 }
-
-
 
 Page.getLayout = function getLayout(page) {
   return <CategoriesMenu>{page}</CategoriesMenu>;
